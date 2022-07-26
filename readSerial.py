6a318ef8014aa8d56372e6099601c1417676c304
Heudiini <77001762+Heudiini@users.noreply.github.com>
2022-07-27 00:04:39 +0300

    Serial -> readSerial

--- readSerial.py
+++ readSerial.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 Copyright (c) 2018 Jani Mantynen & Ville Heikkiniemi
+
 Permission is granted to copy, distribute and/or modify this document under
 the terms of the GNU Free Documentation License, Version 1.3 or any later
 version published by the Free Software Foundation; with no Invariant Sections,
@@ -11,7 +12,9 @@ found online at http://www.fsf.org/licensing/licenses/fdl.txt
 import cgitb
 cgitb.enable()
 
-from serial import Serial
+
+import readSerial
+#from serial import Serial
 from datetime import datetime
 import time
 import json
@@ -63,16 +66,16 @@ def Tapahtuma(data):
             print('Ei pysty kirjoittamaan')
 
 def main():  # Ohjelman suoritus alkaa tästä
-	# Yritetäänn luoda sarjaporttisoketti
+	
     try:
         
-        microbit = Serial(port= "/dev/ttyACM0", baudrate=115200, timeout=2)
+        microbit = readSerial(port= "/dev/ttyACM0", baudrate=115200, timeout=2)
         #global microbit
+        #
     except Exception:
         print('Tarkista portti')
-	# Odotetaan sarjaporttiliikennettä Kuuntelee koko ajan ja iäti 
-	#	sarjaporttia
-    while True: # Ikuinen luuppi!
+	# forever loop
+    while True:
        
         try:
             Tapahtuma(microbit.read(2).decode()) 
